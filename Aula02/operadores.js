775a93357811b94c87d3c78be5d7442cf89d623e
Andre Assunção <104846613+AndreAssun@users.noreply.github.com>
2024-09-04 10:36:14 -0300

    Update operadores.js

--- Aula02/operadores.js
+++ Aula02/operadores.js
@@ -10,9 +10,9 @@ var nota3 = 9;
 var media = (nota1 + nota2 + nota3) / 3
 
 if (media < 3) {
-    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta reprovado`)
+    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta REPROVADO`)
 } else if (media >= 3 && media <= 6) { //operador &&, também conhecido como "AND" (e)
-    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta de recuperação, procure informações com a diretoria !`)
+    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta de RECUPERAÇÃO, procure informações com a diretoria !`)
 } else {
-    console.log(`Sua média é ${media.toFixed(1)} \nParabéns, voce esta Aprovado, boas férias :)`)
+    console.log(`Sua média é ${media.toFixed(1)} \nParabéns, voce esta APROVADO, boas férias :)`)
 }
