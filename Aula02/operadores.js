bf37c7e88ea1e3555fb728573d7fdaebc1e892a3
Andre Assunção <104846613+AndreAssun@users.noreply.github.com>
2024-09-04 10:09:40 -0300

    Update operadores.js
    correção de código

--- Aula02/operadores.js
+++ Aula02/operadores.js
@@ -3,16 +3,16 @@
 //O operador ||, conhecido como "OR" (ou), retorna verdadeiro se pelo menos UMA das expressões fornecidas for verdadeira.
 //Por outro lado, o operador &&, também conhecido como "AND" (e), testa se as DUAS (ou mais) expressões comparadas são veridadeiras.
 
+
 var nota1 = 6;
-var nota2 = 6;
-var nota3 = 7;
-var media = (nota1 + nota2 + nota3) / 3;
+var nota2 = 7;
+var nota3 = 9;
+var media = (nota1 + nota2 + nota3) / 3
 
 if (media < 3) {
-  console.log(`sua media é ${media.toFixed(2)} voce foi Reprovado`);
-} else if (media >= 3 && media <= 6) {  //operador &&, também conhecido como "AND" (e)
-  console.log(`sua media é ${media.toFixed(2)} voce esta de Recuperação`);
-}
- else {
-  console.log(`sua media é ${media.toFixed(2)} \nvoce foi Aprovado`);
+    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta reprovado`)
+} else if (media >= 3 && media <= 6) { //operador &&, também conhecido como "AND" (e)
+    console.log(`Sua média é ${media.toFixed(1)} \nVoce esta de recuperação, procure informações com a diretoria !`)
+} else {
+    console.log(`Sua média é ${media.toFixed(1)} \nParabéns, voce esta Aprovado, boas férias :)`)
 }
